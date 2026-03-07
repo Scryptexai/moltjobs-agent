25469e58e898d4af625da70e02acdfb90694de44
root <root@localhost.localdomain>
2026-03-07 03:24:44 +0000

    Convert webhook to CommonJS

--- api/webhooks/moltjobs.js
+++ api/webhooks/moltjobs.js
@@ -1,5 +1,5 @@
-// Vercel serverless function for webhook
-import axios from 'axios';
+// Vercel serverless function for webhook (CommonJS)
+const axios = require('axios');
 
 async function executeWithLLM(job) {
   const url = process.env.ANTHROPIC_BASE_URL;
@@ -33,7 +33,7 @@ async function executeWithLLM(job) {
   };
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Only accept POST
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
@@ -89,4 +89,4 @@ export default async function handler(req, res) {
       console.error('Webhook error:', e.message);
     }
   }
-}
+};
